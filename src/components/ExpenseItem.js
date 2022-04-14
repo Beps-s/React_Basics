6228808039329c347ad5653cb4677a5ad699f030
Chris Erman <chris.erman@khk.ee>
2022-04-14 14:25:54 +0300

    ExpenseItem.js to display date correctly

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,16 @@
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
+    const month = props.expenseDate.toLocaleString('en-US', { month: 'long' });
+    const day = props.expenseDate.toLocaleString('en-US', { day: '2-digit' });
+    const year = props.expenseDate.getFullYear();
     return (
         <div className="expense-item">
-            <div>{props.expenseDate.toISOString()}</div>
+            <div>
+                <div>{year}</div>
+                <div>{month}</div>
+                <div>{day}</div>
+            </div>
             <div className="expense-item__description">
                 <h2>{props.expenseDesc}</h2>
                 <div className="expense-item__price">${props.expenseSum}</div>
