388a5b43732149ceca55a4f87928b95d8c1a8953
Chris Erman <chris.erman@khk.ee>
2022-04-14 14:19:15 +0300

    Passing ExpenseItem data via props

--- src/App.js
+++ src/App.js
@@ -4,10 +4,53 @@ import './App.css';
 import ExpenseItem from './components/ExpenseItem';
 
 const App = () => {
+  const expenses = [
+    {
+      id: 'e1',
+      expenseDesc: 'Toilet Paper',
+      expenseSum: 94.12,
+      expenseDate: new Date(2020, 7, 14),
+    },
+    { id: 'e2',
+    expenseDesc: 'New TV',
+      expenseSum: 799.49,
+      expenseDate: new Date(2021, 2, 12) },
+    {
+      id: 'e3',
+      expenseDesc: 'Car Insurance',
+      expenseSum: 294.67,
+      expenseDate: new Date(2021, 2, 28),
+    },
+    {
+      id: 'e4',
+      expenseDesc: 'New Desk (Wooden)',
+      expenseSum: 450,
+      expenseDate: new Date(2021, 5, 12),
+    },
+  ];
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem />
+      <ExpenseItem
+        expenseDesc={expenses[0].expenseDesc}
+        expenseSum={expenses[0].expenseSum}
+        expenseDate={expenses[0].expenseDate}
+      ></ExpenseItem>
+      <ExpenseItem
+        expenseDesc={expenses[1].expenseDesc}
+        expenseSum={expenses[1].expenseSum}
+        expenseDate={expenses[1].expenseDate}
+      ></ExpenseItem>
+      <ExpenseItem
+        expenseDesc={expenses[2].expenseDesc}
+        expenseSum={expenses[2].expenseSum}
+        expenseDate={expenses[2].expenseDate}
+      ></ExpenseItem>
+      <ExpenseItem
+        expenseDesc={expenses[3].expenseDesc}
+        expenseSum={expenses[3].expenseSum}
+        expenseDate={expenses[3].expenseDate}
+      ></ExpenseItem>
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,19 +1,15 @@
 import './ExpenseItem.css';
 
-
-const ExpenseItem = () => {
-    const expenseDate = new Date(2022, 6, 18);
-    const expenseDesc =  'Car Insurance';
-    const expenseSum = 185.33;
+const ExpenseItem = (props) => {
     return (
         <div className="expense-item">
-          <div>{expenseDate.toISOString()}</div>
-          <div className="expense-item__description">
-            <h2>{expenseDesc}</h2>
-            <div className="expense-item__price">${expenseSum}</div>
-          </div>
+            <div>{props.expenseDate.toISOString()}</div>
+            <div className="expense-item__description">
+                <h2>{props.expenseDesc}</h2>
+                <div className="expense-item__price">${props.expenseSum}</div>
+            </div>
         </div>
-      );
+    );
 }
 
 export default ExpenseItem;
